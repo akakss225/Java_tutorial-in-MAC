53e5d3823c61261b4e05883b82f5ff030fa91a2b
akakss225 <akakss225@naver.com>
2021-07-15 16:34:03 +0900

    생활코딩4

---
+++ LifeCoding/src/LoopApp.java
@@ -0,0 +1,17 @@
+
+public class LoopApp {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i < 5; i++) {
+			System.out.println(i);
+		}
+		int a = 1;
+		while(a < 5) {
+			System.out.println(a);
+			a++;
+		}
+
+	}
+
+}
